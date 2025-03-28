0c21054cec83e8ace6f54af3ca9d67e442a8d045
kelvin ngene <162456111+kelvin227@users.noreply.github.com>
2025-03-28 06:19:20 +0100

    first commit

---
+++ src/layouts/dashboard/top-nav.jsx
@@ -0,0 +1,61 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Avatar, Box, Button, Link, Stack } from '@mui/material';
+import { Logo } from 'src/components/logo';
+
+const TOP_NAV_HEIGHT = 64;
+
+export const TopNav = () => (
+  <Box
+    component="header"
+    sx={{
+      backgroundColor: 'neutral.900',
+      color: 'common.white',
+      position: 'fixed',
+      width: '100%',
+      zIndex: (theme) => theme.zIndex.appBar
+    }}
+  >
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      sx={{
+        minHeight: TOP_NAV_HEIGHT,
+        px: 3
+      }}
+    >
+      <Stack
+        alignItems="center"
+        direction="row"
+        spacing={3}
+      >
+        <Box
+          component={RouterLink}
+          to="/"
+          sx={{
+            display: 'inline-flex',
+            height: 24,
+            width: 24
+          }}
+        >
+          <Logo />
+        </Box>
+      </Stack>
+      <Stack
+        alignItems="center"
+        direction="row"
+        spacing={2}
+      >
+        <Button
+        color="primary"
+        size="large"
+        variant="contained">
+          become a merchant
+        </Button>
+        <Avatar
+          src="/assets/avatars/avatar-chen-simmons.jpg"
+          variant="rounded"
+        />
+      </Stack>
+    </Stack>
+  </Box>
+);
